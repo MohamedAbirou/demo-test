60f3ccf0d904aadf0a7ea4344acd6859c189a7cc
Mohamed Abirou <abiroumohamed58@gmail.com>
2026-02-03 18:04:44 +0100

    Configure Tailwind CSS with custom theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import tailwind from "tailwindcss";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
 })
